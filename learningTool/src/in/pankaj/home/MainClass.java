5f9dec73148b6c09e517da30e749e465b9cbbabf
pankaj <pankaj@kalilinux>
2017-04-08 14:30:48 +0530

    first load to repos

---
+++ learningTool/src/in/pankaj/home/MainClass.java
@@ -0,0 +1,12 @@
+package in.pankaj.home;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to tool home !");
+		System.out.println("Welcome to tool home once again !");
+
+	}
+
+}
